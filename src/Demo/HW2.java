b0232a557e1bd88fa9dd87dfc9abfee6fd4607e6
chuchu <chuchu@rands-air.lan>
2025-01-30 10:32:42 -0500

    New code

---
+++ src/Demo/HW2.java
@@ -0,0 +1,15 @@
+package Demo;
+
+public class HW2 {
+	public class DemoTest3 {
+		public static void main(String args[]) {
+
+			int a = 5;
+			int b = 7;
+
+			int result = (a + b);
+			System.out.println(result);
+
+		}
+	}
+}
